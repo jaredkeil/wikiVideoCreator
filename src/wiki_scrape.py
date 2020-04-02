cf3a74d0e38d6cd0b22aa2ee6bc679e017e3e665
Jared Keil <jared@Jareds-MacBook-Pro.local>
2020-04-02 16:53:38 -0700

    moviemaker formatting

--- src/wiki_scrape.py
+++ src/wiki_scrape.py
@@ -7,3 +7,6 @@ def parse_wiki(page_name):
     ### Here would be the place to parse script
     print("Video Title: ", title)
     return title, script
+
+if __name__ == "__main__":
+    parse_wiki("Web Scraping")
