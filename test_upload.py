14bbba119ef8f747dd5746d36802323281f0111d
Jared Keil <jared@Jareds-MacBook-Pro.local>
2021-01-21 15:41:34 -0800

    helpful functoin comments and argparse descriptions

--- test_upload.py
+++ test_upload.py
@@ -4,6 +4,8 @@ import subprocess
 
 
 def upload_to_youtube(filepath):
+    # Upload to youtube privately with simple arguments.
+    # Requires access/oauth2 tokens -> client_secrets.json & upload_video.py-oauth2.json
     p = Path(filepath)
     arglist = ['python', 'upload_video.py',
             f'--file={p.absolute()}',
@@ -11,14 +13,14 @@ def upload_to_youtube(filepath):
             f'--description={p.stem}',
             f'--keywords=',
             '--category=27',
-            '--privacyStatus=private',
-            '--noauth_local_webserver']
+            '--privacyStatus=private',  # You can change privacyStatus to 'public' if desired.
+            '--noauth_local_webserver'] # I believe necessary for browserless. Without, browser pops-up and asks permission.
     print(arglist)
     subprocess.run(arglist)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Upload videos to youtube. Usage: python test_upload.py <filepath>')
     parser.add_argument('filepath')
     opt = parser.parse_args()
     upload_to_youtube(filepath=opt.filepath)
