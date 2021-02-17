d3144aa4620a3b2e7d2977913fa8397548c65cdd
Jared Keil <jaredrkeil@gmail.com>
2021-02-16 18:40:27 -0800

    reformat of uplaod test, util test creation

--- tests/unit/test_upload.py
+++ tests/unit/test_upload.py
@@ -7,16 +7,16 @@ def upload_to_youtube(filepath):
     # Upload to youtube privately with simple arguments.
     # Requires access/oauth2 tokens -> client_secrets.json & upload_video.py-oauth2.json
     p = Path(filepath)
-    arglist = ['python', 'upload_video.py',
-            f'--file={p.absolute()}',
-            f'--title={p.stem}',
-            f'--description={p.stem}',
-            f'--keywords=',
-            '--category=27',
-            '--privacyStatus=private',  # You can change privacyStatus to 'public' if desired.
-            '--noauth_local_webserver'] # I believe necessary for browserless. Without, browser pops-up and asks permission.
-    print(arglist)
-    subprocess.run(arglist)
+    args = ['python', 'upload_video.py',
+               f'--file={p.absolute()}',
+               f'--title={p.stem}',
+               f'--description={p.stem}',
+               f'--keywords=',
+               '--category=27',
+               '--privacyStatus=private',  # You can change privacyStatus to 'public' if desired.
+               '--noauth_local_webserver']  # For browser-less run. Without, browser opens and asks permission.
+    print(args)
+    subprocess.run(args)
 
 
 if __name__ == '__main__':
---
+++ tests/unit/test_utils.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from wiki_movie.utils import write_seq_to_file, make_directory, file_len, repository_root
+
+
+class UtilsTest(TestCase):
+    def test_repository_root(self):
+        found_repo = repository_root.stem
+        self.assertEqual(found_repo, 'video-creater')
+
