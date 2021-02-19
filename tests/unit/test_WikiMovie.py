dd59fb00005f8017602c20d5aa0868c8f8e66949
Jared Keil <jaredrkeil@gmail.com>
2021-02-18 17:00:50 -0800

    new wiki movie class testing init

---
+++ tests/unit/test_WikiMovie.py
@@ -0,0 +1,47 @@
+from unittest import TestCase, mock, skipUnless
+import pkgutil
+
+from wiki_movie.movie_maker2 import WikiMovie
+from wiki_movie.utils import repository_root
+
+class WikiMovieTest(TestCase):
+
+    @mock.patch('sys.platform', 'darwin')
+    def test_create_darwin_platform_default_narrator(self):
+        w_movie = WikiMovie(title='Test')
+        narrator_cls_name = w_movie.narrator.__class__.__name__
+        self.assertEqual('SystemNarrator', narrator_cls_name)
+
+    @mock.patch('sys.platform', 'linux')
+    @skipUnless(pkgutil.find_loader('pyttsx3'), 'pyttsx3 not installed')
+    def test_create_other_platform_default_narrator(self):
+        w_movie = WikiMovie(title='Test')
+        narrator_cls_name = w_movie.narrator.__class__.__name__
+        self.assertEqual('PyttsNarrator', narrator_cls_name)
+
+    @skipUnless(pkgutil.find_loader('tensorflow'), 'Tensorflow not installed')
+    def test_create_with_narrator_name(self):
+        w_movie = WikiMovie(title='Test', narrator_name='dc_tts')
+        narrator_cls_name = w_movie.narrator.__class__.__name__
+        self.assertEqual('DcttsNarrator', narrator_cls_name)
+
+    def test_create_default_downloader(self):
+        w_movie = WikiMovie(title='Test')
+        downloader = w_movie.image_downloader
+
+        self.assertEqual(downloader.main_keyword, 'Test')
+        self.assertEqual(downloader.url_dir, repository_root / 'data' / 'url_files')
+
+    def test_pass_downloader_args(self):
+        w_movie = WikiMovie(title='Test',
+                            downloader_args={'img_dir': 'test_path',
+                                             'supplemented_keywords': ['1', '2', '3']
+                                             }
+                            )
+        downloader = w_movie.image_downloader
+
+        expected_keywords = [' ', 'Arts and entertainment', 'Computing', 'People', 'Science and technology', 'Sports', 'Other uses']
+
+        self.assertEqual(downloader.img_dir, 'test_path')
+        self.assertEqual(downloader.main_keyword, 'Test')
+        self.assertListEqual(downloader.supplemented_keywords, ['1', '2', '3'])
--- wiki_movie/movie_maker2.py
+++ wiki_movie/movie_maker2.py
@@ -1,9 +1,21 @@
 import importlib
 import sys
 
+from wikipediaapi import Wikipedia
+
+from wiki_movie.text import generate_script_list
+from wiki_movie.utils import repository_root
+from wiki_movie.image_downloader import ImageDownloader
+
+w_api = Wikipedia('en')
+
 
 class WikiMovie:
-    def __init__(self, narrator_name='', narrator_args=None):
+    def __init__(self, title, narrator_name='', narrator_args=None, downloader_args=None):
+        self.title = title
+        self.script = generate_script_list(w_api.page(title))
+
+        # Set up narrator
         if not narrator_name:
             if sys.platform == 'darwin':
                 narrator_name = 'sys_tts'
@@ -14,14 +26,21 @@ class WikiMovie:
             narrator_args = {}
 
         module = importlib.import_module(f'wiki_movie.narrators.{narrator_name}')
-        self.narrator = module.build_narrator(narrator_args)
+        self.narrator = module.build_narrator(self.script, narrator_args)
 
+        # Set up image downloader
+        keywords = [' '] + [sd['title'] for sd in self.script[1:] if sd['text']]
+        _dir = repository_root / 'data'
 
+        if not downloader_args:
+            downloader_args = {}
 
+        d_args = dict(main_keyword=title, supplemented_keywords=keywords,
+                      url_dir=_dir / 'url_files', img_dir=_dir / 'images')
+        d_args.update(downloader_args)
 
+        self.image_downloader = ImageDownloader(**d_args)
 
     def make_movie(self):
-
-
 
         self.narrator.make_narration(self.script, self.audio_dir)
