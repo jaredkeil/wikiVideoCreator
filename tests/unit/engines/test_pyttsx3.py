8180a842103b120c813ab23e8e8f257419e549d1
Jared Keil <jaredrkeil@gmail.com>
2021-02-19 21:02:02 -0800

    skip test which require sound ouput, skip wait test

--- tests/unit/engines/test_pyttsx3.py
+++ tests/unit/engines/test_pyttsx3.py
@@ -1,16 +1,18 @@
 import time
 import os
+from unittest import skip
 
 import pyttsx3
 from wiki_movie.utils import localtime_filepath
 from tests.unit.engines.engine_base import BaseEngineTest
 
 
-class PyttsxTest(BaseEngineTest):
+class Pyttsx3Test(BaseEngineTest):
     def setUp(self):
         super().setUp()
         self.engine = pyttsx3.init()
 
+    @skip('Sound card verification is not implemented yet.')
     def test_say(self):
         text = 'Hello world'
         self.engine.say(text)
@@ -34,6 +36,7 @@ class PyttsxTest(BaseEngineTest):
         self.engine.save_to_file(text, file_path)
         self.engine.runAndWait()
 
+    @skip('Skip because timeout is likely')
     def test_save_with_wait(self):
         text = 'If there is no audio after this sentence, the test failed. If hearing this then test passed.'
         text = ' '.join(text + f' {i} loops complete.' for i in range(5))
