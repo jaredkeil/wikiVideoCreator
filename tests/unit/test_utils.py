f33fd89df642f9ef7eac671e9ea42f162217d177
Jared Keil <jaredrkeil@gmail.com>
2021-02-22 10:09:29 -0800

    repository root name changed to wikiVideoCreator

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -6,5 +6,5 @@ from wiki_movie.utils import write_seq_to_file, make_directory, file_len, reposi
 class UtilsTest(TestCase):
     def test_repository_root(self):
         found_repo = repository_root.stem
-        self.assertEqual(found_repo, 'video-creater')
+        self.assertEqual(found_repo, 'wikiVideoCreator')
 
