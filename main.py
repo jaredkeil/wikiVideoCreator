2d69a623d1dd057041c4fc55b63c6fcedb1b95f9
Jared Keil <jaredrkeil@gmail.com>
2021-02-27 14:19:27 -0800

    main.py initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+import argparse
+import subprocess
+
+from wiki_movie.upload_video import __file__ as uploader
+from wiki_movie.video import WikiMovie, parse_narrator_args, parse_downloader_args
+from wiki_movie.resources import top25, wiki_page_list
+
+
+def upload(video_path, title, description, private=False):
+    privacy = 'private' if private else 'public'
+
+    cmd_args = ['python', uploader,
+                f'--file={video_path}',
+                f'--title={title}',
+                f'--description={description}',
+                f'--keywords=',
+                '--category=27',
+                f'--privacyStatus={privacy}',
+                '--noauth_local_webserver']
+
+    print(cmd_args)
+
+    subprocess.run(cmd_args)
+
+
+def main(args):
+    if args.single_page:
+        titles = [" ".join(args.single_page)]
+
+    elif args.top25:
+        titles = top25()
+
+    elif args.url:
+        titles = wiki_page_list(args.url, args.n_pages)
+
+    for title in titles:
+        movie = WikiMovie(title, args.narrator, parse_narrator_args(args), parse_downloader_args(args), args.overwrite)
+        movie.make_movie(args.overwrite)
+
+        if args.upload:
+            upload(movie.vid_path, movie.script[0]['title'], movie.script[0]['text'], args.private)
+
+        if args.delete_all:
+            movie.delete_assets()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='make video and optionally upload via YouTube API.')
+
+    # Required arguments
+    mode = parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument('-s', '--single_page', nargs='+', help='name of one specific page')
+    mode.add_argument('-t', '--top25', action='store_true')
+    mode.add_argument('--url', help='URL of an article, which must contain a list/table of articles')
+
+    # Optional arguments
+    parser.add_argument('-d', '--delete_all', action='store_true', help='delete assets after movie is made')
+    parser.add_argument('-n', '--n_pages', type=int, default=25, help='number to process from url')
+
+    parser.add_argument('-u', '--upload', action='store_true', help='upload to YouTube')
+    parser.add_argument('-p', '--private', action='store_true', help='set uploaded video as private')
+    parser.add_argument('-o', '--overwrite', action='store_true', help='overwrite previous files under same title')
+    parser.add_argument('-n', '--narrator', default='sys_tts', choices=('sys_tts', 'google_tts', 'py_tts', 'dc_tts'),
+                        help='Possible narrator names: sys_tts, py_tts, google_tts')
+
+    # WikiMovie/ImageDownloader object arguments
+    narrator_args = parser.add_argument_group('narrator arguments (optional)')
+    narrator_args.add_argument('-v', '--voice', help='voice of sys_tts - available voices dependant on platform')
+    narrator_args.add_argument('-r', '--rate', help='words per minute of sys_tts')
+
+    downloader_args = parser.add_argument_group('image downloader arguments (optional)')
+    downloader_args.add_argument('-i', '--image_count', type=int, help='how many images per section')
+    downloader_args.add_argument('-c', '--connect_speed', choices=('very slow', 'slow', 'medium', 'fast', 'very fast'),
+                                 help='how fast the webdriver clicks on search results')
+    downloader_args.add_argument('-w', '--watch', action='store_true', help='run browser in non-headless mode')
+
+    options = parser.parse_args()
+    print(options)
+    main(options)
