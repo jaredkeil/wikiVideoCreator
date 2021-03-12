a24738e6261a5922abceb1136a0ecb73be81a3d0
Jared Keil <jaredrkeil@gmail.com>
2021-03-12 11:36:50 -0800

    formatting/syntax image modules

--- wiki_movie/image/downloader.py
+++ wiki_movie/image/downloader.py
@@ -1,12 +1,3 @@
-####################################################################################################################
-# Download images from google with specified keywords for searching
-# search query is created by "main_keyword + supplemented_keyword"
-# if there are multiple keywords, each main_keyword will join with each supplemented_keyword
-# Use selenium and urllib, and each search query will download any number of images that google provides
-# allow single process or multiple processes for downloading
-# Pay attention that since selenium is used, geckodriver and firefox browser is required
-####################################################################################################################
-
 import os
 from pathlib import Path
 import sys
@@ -18,8 +9,8 @@ from urllib.parse import urlparse, quote
 from user_agent import generate_user_agent
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import ElementNotInteractableException, TimeoutException, \
-    ElementClickInterceptedException
+from selenium.common.exceptions import ElementNotInteractableException, \
+    TimeoutException, ElementClickInterceptedException
 from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as EC
 from selenium.webdriver.remote.webelement import WebElement
@@ -29,24 +20,33 @@ from wiki_movie.utils import write_seq_to_file, make_directory, file_len
 
 
 class ImageDownloader:
-    def __init__(self, main_keyword, supplemented_keywords=None, url_dir=None, img_dir=None,
-                 num_requested=5, connection_speed="medium", headless=True):
+    def __init__(self,
+                 main_keyword,
+                 supplemented_keywords=None,
+                 url_dir=None,
+                 img_dir=None,
+                 num_requested=5,
+                 connection_speed="medium",
+                 headless=True):
         """
         Args:
             main_keyword (str): main keyword
             supplemented_keywords (list[str]): list of supplemented keywords
-            url_dir (Path): parent directory where the url.txt files are stored to folder called <main_keyword>
-            img_dir (Path): parent directory where images will be downloaded to folder called <main_keyword>
+            url_dir (Path): parent directory where the url.txt files are stored
+            img_dir (Path): parent directory where images will be downloaded
             num_requested (int): maximum number of images to download
-            connection_speed (str): 'very slow', 'slow', 'medium', 'fast', or 'very fast'
+            connection_speed (str): very slow, slow, medium, fast, or very fast
         """
         self.main_keyword = main_keyword
         self.supplemented_keywords = supplemented_keywords if supplemented_keywords else []
         self.url_dir = url_dir if url_dir else Path.cwd() / 'data' / 'url_files'
         self.img_dir = img_dir if img_dir else Path.cwd() / 'data' / 'images' / main_keyword
         self.num_requested = num_requested
-        self.wait_time = {'very slow': 7, 'slow': 3, 'medium': 1, 'fast': 0.5, 'very fast': .25
-                          }.get(connection_speed, 1)
+        self.wait_time = {'very slow': 7,
+                          'slow': 3,
+                          'medium': 1,
+                          'fast': 0.5,
+                          'very fast': .25}.get(connection_speed, 1)
         self.headless = headless
         self.img_urls = set()
         self._boot_driver()
@@ -85,12 +85,13 @@ class ImageDownloader:
         print(f"Wrote {len(urls)} links to {link_file_path}\n")
 
     def _search_for_images(self, keyword):
-        print(f'Performing image search for "{self.main_keyword + " " + keyword}"')
+        print(f'Searching for "{self.main_keyword + " " + keyword}"')
         formatted_query = quote(self.main_keyword + ' ' + keyword)
-        self.driver.get("https://www.google.com/search?q=" + formatted_query + "&source=lnms&tbm=isch")
+        self.driver.get("https://www.google.com/search?q=" + formatted_query
+                        + "&source=lnms&tbm=isch")
 
     def _show_more_results(self):
-        """Scroll down the page to load more image results. Click on 'More results' if possible"""
+        # Scroll, loading more image results. Click 'More results' if possible
         try:
             more_results_element = WebDriverWait(self.driver, 1).until(
                 EC.element_to_be_clickable(GoogleImagesLocators.MORE_RESULTS))
@@ -98,13 +99,13 @@ class ImageDownloader:
         except (TimeoutException, ElementNotInteractableException):
             pass
 
-        # Scroll
         self.driver.execute_script("window.scrollBy(0, 1000)")
 
     def _get_thumbnails(self):
         try:
             thumbnails = WebDriverWait(self.driver, 10).until(
-                n_elements_clickable(GoogleImagesLocators.THUMBNAILS, self.num_requested)
+                n_elements_clickable(GoogleImagesLocators.THUMBNAILS,
+                                     self.num_requested)
             )
             return set(thumbnails)
         except TimeoutException:
@@ -115,9 +116,9 @@ class ImageDownloader:
         n_found = 0
         i = 0
         while len(urls) < self.num_requested and i < len(thumbs):
-            # print(f"Finding URL's [{'#' * (n_found + 1) + ' ' * (self.num_requested - n_found)}]   \r")
             sys.stdout.write(
-                f"Finding {self.num_requested} URL's [{'#' * n_found + ' ' * (self.num_requested - n_found)}]   \r")
+                f"Finding {self.num_requested} URL's "
+                f"[{'#' * n_found + ' ' * (self.num_requested - n_found)}] \r")
             sys.stdout.flush()
             src = self._extract_src_from_thumb(thumbs[i])
             if src:
@@ -128,13 +129,17 @@ class ImageDownloader:
 
     def _extract_src_from_thumb(self, thumb):
         try:
-            WebDriverWait(self.driver, self.wait_time).until(element_is_clickable(thumb)).click()
-        except (TimeoutException, ElementNotInteractableException, ElementClickInterceptedException) as e:
+            WebDriverWait(self.driver, self.wait_time).until(
+                element_is_clickable(thumb)).click()
+        except (TimeoutException, ElementNotInteractableException,
+                ElementClickInterceptedException) as e:
             logging.error(f'Error clicking thumbnail: {e}')
 
         try:
-            elements = self.driver.find_elements(*GoogleImagesLocators.URLS_IN_THUMBS)
-            el = WebDriverWait(self.driver, self.wait_time).until(element_is_clickable(elements[1]))
+            elements = self.driver.find_elements(
+                *GoogleImagesLocators.URLS_IN_THUMBS)
+            el = WebDriverWait(self.driver, self.wait_time).until(
+                element_is_clickable(elements[1]))
             src = el.get_attribute('src')
             if src.startswith('http') and not src.startswith('https://encrypted-tbn0.gstatic.com'):
                 return {src}
@@ -170,11 +175,13 @@ class ImageDownloader:
             self._download_image_from_keyword(keyword)
 
             downloaded_num = len(os.listdir(sk_img_dir))
-            print(f"\nSuccessfully downloaded {downloaded_num} images for search '{self.main_keyword + ' ' + keyword}'.\n")
+            print(f"\nSuccessfully downloaded {downloaded_num} images "
+                  f"for search '{self.main_keyword + ' ' + keyword}'.\n")
 
     def _download_image_from_keyword(self, keyword):
         """
-        Reads urls from '{keyword}.txt', and downloads to images/main_keyword/supp_keyword/ directory
+        Reads urls from '{keyword}.txt'.
+        Downloads to images/main_keyword/supp_keyword/ directory
         """
         print(f"Downloading path: {self._define_supp_img_dir(keyword)}")
 
@@ -185,7 +192,8 @@ class ImageDownloader:
             for i, link in enumerate(rf.readlines()):
                 # print(link)
                 sys.stdout.write(
-                    f"Downloading {n_links} images [{'#' * (i + 1) + ' ' * (n_links - i - 1)}]   \r")
+                    f"Downloading {n_links} images "
+                    f"[{'#' * (i + 1) + ' ' * (n_links - i - 1)}]   \r")
                 sys.stdout.flush()
                 self._get_link(link, keyword)
 
@@ -194,11 +202,14 @@ class ImageDownloader:
             self._save_link(link, keyword)
             self.img_path += 1
         except urllib.error.HTTPError as e:
-            logging.error(f'HTTPError while downloading image {link}. http code {e.code}, reason:{e.reason}')
+            logging.error(f'HTTPError while downloading image {link}. '
+                          f'http code {e.code}, reason:{e.reason}')
         except urllib.error.URLError as e:
-            logging.error(f'URLError while downloading image {link}. Reason:{e.reason}')
+            logging.error(f'URLError while downloading image {link}. '
+                          f'Reason:{e.reason}')
         except Exception as e:
-            logging.error(f'Unexpected error while downloading image {link}. error type:{e.args}')
+            logging.error(f'Unexpected error while downloading image {link}. '
+                          f'error type:{e.args}')
 
     def _save_link(self, link, keyword):
         response = self._req_response(link)
@@ -211,15 +222,15 @@ class ImageDownloader:
         parsed = urlparse(url)
         headers = {
             'User-Agent': generate_user_agent(),
-            'referer': parsed.scheme + '://' + parsed.hostname  # ex. referer = 'https://www.google.com'
-        }
+            'referer': parsed.scheme + '://' + parsed.hostname
+        }  # ex. referer = 'https://www.google.com'
         req = urllib.request.Request(url.strip(), headers=headers)
         return urllib.request.urlopen(req, timeout=10)
 
 
 class n_elements_clickable:
     """
-    Expected condition to check whether there are a certain number of elements found by the locator
+    Check whether there are a certain number of elements found by the locator
     """
     def __init__(self, locator, n):
         self.locator = locator
--- wiki_movie/image/utils.py
+++ wiki_movie/image/utils.py
@@ -2,47 +2,51 @@ import cv2
 from wiki_movie.utils import make_directory
 
 
-def maxsize_pad(img_path, save_path, desired_width=1920, desired_height=1080):
+def maxsize_pad(img_path, save_path, width=1920, height=1080):
     """
-    Creates new image of maximum size within shape(desired_width, desired_height),
-    while maintaining aspect ratio. Adds black border on unused pixels to fill out shape.
+    Save image of max size within width x height, keeping aspect ratio.
+    Adds black border on unused pixels to fill out shape.
     """
     im = cv2.imread(img_path)
 
     old_size = im.shape[:2]  # old_size is in (height, width) format
-    ratio = min(float(desired_width)/im.shape[1], float(desired_height)/im.shape[0])
+    ratio = min(float(width) / im.shape[1],
+                float(height) / im.shape[0])
     new_size = tuple([int(x*ratio) for x in old_size])
     # new_size is in (width, height) format
 
     im = cv2.resize(im, (new_size[1], new_size[0]))
 
-    delta_w = desired_width - new_size[1]
-    delta_h = desired_height - new_size[0]
+    delta_w = width - new_size[1]
+    delta_h = height - new_size[0]
 
     top, bottom = delta_h//2, delta_h-(delta_h//2)
     left, right = delta_w//2, delta_w-(delta_w//2)
 
     black = [0, 0, 0]  # color used in border
 
-    new_im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=black)
+    new_im = cv2.copyMakeBorder(im, top, bottom, left, right,
+                                cv2.BORDER_CONSTANT, value=black)
 
     cv2.imwrite(save_path, new_im)
 
 
 def resize_subdirectory(s_img_dir, resized_images_dir):
-    file_names = [x.parts[-1] for x in s_img_dir.glob('*') if x.is_file() and x.parts[-1][0] != '.']
+    file_names = [x.parts[-1] for x in s_img_dir.glob('*')
+                  if x.is_file() and x.parts[-1][0] != '.']
 
     for file_name in file_names:
         try:
-            maxsize_pad(str(s_img_dir / file_name), str(resized_images_dir / file_name))
+            maxsize_pad(str(s_img_dir / file_name),
+                        str(resized_images_dir / file_name))
         except Exception as e:
             print(f'{e} saving resized image {file_name}, e')
 
 
 def resize_image_directory(img_dir, out_dir):
     """
-    img_dir (Path) -- directory to walk through, resizing images in subdirectories
-    out_dir (Path) -- directory to save resized images in new subdirectories, named like original
+    img_dir (Path) -- directory to recursively search, resize images
+    out_dir (Path) -- directory to save resized images in new subdirectories
     """
     for p in img_dir.iterdir():
         if p.is_dir():
