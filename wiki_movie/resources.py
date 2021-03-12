636201d28b63df3afbe743b8a0ba273493c39603
Jared Keil <jaredrkeil@gmail.com>
2021-03-12 10:51:59 -0800

    raising error if article column parameter not found in tables at url

--- wiki_movie/resources.py
+++ wiki_movie/resources.py
@@ -6,12 +6,17 @@ W_API = Wikipedia('en')
 
 def wiki_page_list(url, n_pages=None, article_column='Article'):
     tables = pd.read_html(url)
+    selected_table = None
     for table in tables:
         if article_column in table:
+            selected_table = table
             break
+    if selected_table is None:
+        raise KeyError(f'No column "{article_column}" found in any tables at {url}.')
+
     if n_pages is None:
-        n_pages = table.shape[0]
-    return list(table[article_column][:n_pages])
+        n_pages = selected_table.shape[0]
+    return list(selected_table[article_column][:n_pages])
 
 
 def top25():
